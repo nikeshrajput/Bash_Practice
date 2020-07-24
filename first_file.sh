44b57aa86f224e0337fd10edbe9a8f25eb49522c
Nikesh Singh B <nikeshsingh.bundelkhand@gmail.com>
2020-07-24 12:15:36 +0200

    Using Multiline comments in Bash Scripting

--- first_file.sh
+++ first_file.sh
@@ -1,4 +1,16 @@
 #! /usr/bin/bash
+: '
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below
+this is a comment for cat line written below'
 
-#this is a comment for cat line written below
 cat > file.txt
