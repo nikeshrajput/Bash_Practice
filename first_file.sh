ecd83cf526d8a594d7b8b92c911290c42d8002d3
Nikesh Singh B <nikeshsingh.bundelkhand@gmail.com>
2020-07-24 12:35:41 +0200

    Removing delimeter from the heredoc module

--- first_file.sh
+++ first_file.sh
@@ -3,15 +3,15 @@
 NewFile=output.sh
 
 (
-    cat <<'Addtext'
-    #! /usr/bin/bash
+cat <<'Addtext'
+#! /usr/bin/bash
 
-    echo "This script creates a new file"
-    
-    var1 = 10
-    var2 = 20
+echo "This script creates a new file"
 
-    ((result=$var1*$var2))
-    echo "The result = $result"
-    Addtext
+var1 = 10
+var2 = 20
+
+((result=$var1*$var2))
+echo "The result = $result"
+Addtext
 ) > $NewFile
